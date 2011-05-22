d74791e298305bd202b7b26117daf55ee00cbcbf
Kurt Raschke <kurt@kurtraschke.com>
2011-05-22 15:05:20 -0400

    Use contrasting colors on plot where possible.

--- plot_service.py
+++ plot_service.py
@@ -7,7 +7,7 @@ from matplotlib.backends.backend_pdf import FigureCanvasPdf as FigureCanvas
 from matplotlib.figure import Figure
 
 
-def make_labels(rects, ax):
+def make_labels(rects, ax, color='white'):
     for rect in rects:
         height = int(rect.get_height())
         if (height >= 2):
@@ -15,7 +15,7 @@ def make_labels(rects, ax):
             yloc = rect.get_y() + (rect.get_height() / 2.0)
             xloc = rect.get_x() + (rect.get_width() / 2.0)
             ax.text(xloc, yloc, bar_text, horizontalalignment='center',
-                    verticalalignment='center', color='white',
+                    verticalalignment='center', color=color,
                     weight='bold', size='x-small')
 
 
@@ -44,6 +44,21 @@ def mode_string(route_types):
 
     return "%s per hour" % (vehicle_name)
 
+def contrasting_color(input_color):
+    r = int(input_color[0:2], 16)
+    g = int(input_color[2:4], 16)
+    b = int(input_color[4:6], 16)
+
+    r_scale = r / 255.0
+    g_scale = g / 255.0
+    b_scale = b / 255.0
+
+    if max(r_scale, g_scale, b_scale) > 0.5:
+        (new_r, new_g, new_b) = (0,0,0)
+    else:
+        (new_r, new_g, new_b) = (255,255,255)
+
+    return "{:02x}{:02x}{:02x}".format(new_r, new_g, new_b)
 
 def plot_service(results, target_stop_name, target_date, outfile):
     HOURS = 24
@@ -64,11 +79,11 @@ def plot_service(results, target_stop_name, target_date, outfile):
 
     for route_id, route_data in results.items():
         bar_args = {}
-        if route_data['route_color'] != '':
+        if route_data['route_color'] and route_data['route_color'] != '':
             bar_args['color'] = '#' + route_data['route_color']
         else:
             bar_args['color'] = '#000000'
-            bar_args['edgecolor'] = '#ffffff'
+            bar_args['edgecolor'] = '#444444'
         color_dups[bar_args['color']] += 1
         if color_dups[bar_args['color']] > 1:
             (hatch_dup, hatch_id) = divmod(color_dups[bar_args['color']] - 2,
@@ -79,8 +94,8 @@ def plot_service(results, target_stop_name, target_date, outfile):
         route_data['plot_1'] = ax.bar(pos + width, route_data['bins_1'], width,
                                       bottom=values_1, **bar_args)
 
-        make_labels(route_data['plot_0'], ax)
-        make_labels(route_data['plot_1'], ax)
+        make_labels(route_data['plot_0'], ax, '#' + contrasting_color(bar_args['color'][1:]))
+        make_labels(route_data['plot_1'], ax, '#' + contrasting_color(bar_args['color'][1:]))
 
         values_0 += np.array(route_data['bins_0'])
         values_1 += np.array(route_data['bins_1'])
