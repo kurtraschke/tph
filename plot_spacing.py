d0ff071f6659153c8b0f75ee93d63312648770ee
Kurt Raschke <kurt@kurtraschke.com>
2011-10-03 21:18:44 -0400

    Avoid crash in plot_spacing() if there is no service in an interval.

--- plot_spacing.py
+++ plot_spacing.py
@@ -8,8 +8,12 @@ def plot_spacing(intervals, spacing, worstspacing):
     print 'Service period -  Spacing'
     for i in range(len(intervals)-1):
         # heuristic: if worst spacing is greater than 1.25 x median spacing, print it also
-        spacingstr = str(int(spacing[i]))
-        if float(worstspacing[i]) / float(spacing[i]) > 1.25:
-            spacingstr = spacingstr + ' - ' + str(int(worstspacing[i]))
-        spacingstr = spacingstr + ' min'
-        print '%s - %s ... %s' % (hour_to_time(intervals[i]), hour_to_time(intervals[i+1]), spacingstr)
+        if spacing[i] == float("inf"):
+            spacingstr = "No service"
+        else:
+            spacingstr = str(int(spacing[i]))
+            if float(worstspacing[i]) / float(spacing[i]) > 1.25:
+                spacingstr = spacingstr + ' - ' + str(int(worstspacing[i]))
+            spacingstr = spacingstr + ' min'
+        print '%s - %s ... %s' % (hour_to_time(intervals[i]),
+                                  hour_to_time(intervals[i+1]), spacingstr)
